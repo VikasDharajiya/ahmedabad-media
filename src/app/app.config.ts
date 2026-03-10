dc765c1cb825e32e6518c44e602fe40dbf494ddc
VikasDharajiya <vikasdharajiya18@gmail.com>
2026-03-10 16:26:25 +0530

    add animation in route using withviewtransition in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
@@ -7,7 +7,7 @@ import { providePrimeNG } from 'primeng/config';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     providePrimeNG({
       unstyled: true,
       ripple: true,
